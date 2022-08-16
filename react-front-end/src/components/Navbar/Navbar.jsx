e186f9d2e518bf3408d6d7af990d93b08dab1e15
Marco <marco.salman@gmail.com>
2022-08-15 22:53:01 -0400

    changing button for profile and logout

--- react-front-end/src/components/Navbar/Navbar.jsx
+++ react-front-end/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
+import { styled } from "@mui/material/styles";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { AppContext } from "../hooks/useAppContext";
+import { Button } from "@mui/material";
 
 const logo = require("./TripperLogo.png");
 
@@ -14,6 +16,32 @@ export default function Navbar({ setFormData }) {
 
   // For presentation purposes...Demo
   const username = "Shakespeare";
+
+  const BootstrapButton = styled(Button)({
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 12,
+    padding: "6px 12px",
+    border: "1px solid",
+    marginLeft: "5px",
+    lineHeight: 1.5,
+    backgroundColor: "#0063cc",
+    borderColor: "#0063cc",
+    fontFamily: ["Roboto"].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#0062cc",
+      boxShadow: "none",
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf",
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    },
+  });
 
   return (
     <div className="navbar">
@@ -41,13 +69,25 @@ export default function Navbar({ setFormData }) {
 
       {username && (
         <div className="signOut">
-          <h2>Hello, {user.name}!</h2>
+          <h5>Hello, {user.name}!</h5>
+          <div className = "singOutProfile">
           <Link to="/profile">
-            <button>User Profile</button>
+            <BootstrapButton variant="contained" size="small">
+              User Profile
+            </BootstrapButton>
           </Link>
+          </div>
+          <div className = "signOutLogOut">
           <Link to="/login">
-            <button onClick={clearData}>Log Out</button>
+            <BootstrapButton
+              variant="contained"
+              size="small"
+              onClick={clearData}
+            >
+              Log Out
+            </BootstrapButton>
           </Link>
+          </div>
         </div>
       )}
     </div>
